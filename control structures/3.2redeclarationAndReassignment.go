49a3cbdf71a52d421d64c7e32023d1ad820d0614
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-31 10:05:55 +0530

    indent code

--- control structures/3.2redeclarationAndReassignment.go
+++ control structures/3.2redeclarationAndReassignment.go
@@ -1,16 +1,15 @@
+
+/*   In a := declaration a variable v may appear even if it has already been declared, provided this declaration is in the same scope as the existing declaration
+     of v (if v is already declared in an outer scope , the declaration will create a new variable §) , the corresponding value in the initialization is assignable 
+     to v, and there is at least one other variable that is created by the declaration.   */
+
 package main
 
 import "fmt"
 
 func main(){
-	//the := works as a short declaration form
 	
+	//the := works as a short declaration form
 	x,y:=110,120
 	fmt.Println(x,y)
-
-	/*in a := declaration a variable v may appear even if it has already been declared, provided
-	  * this declaration is in the same scope as the existing declaration of v (if v is already declared in an outer scope, 
-		the declaration will create a new variable §),
-	  * the corresponding value in the initialization is assignable to v, and
-      * there is at least one other variable that is created by the declaration.        */
 }
