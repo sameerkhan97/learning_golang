3533e50d5945070e80899237aa849de5363a121f
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-31 10:08:00 +0530

    indent code

--- control structures/3.4switch.go
+++ control structures/3.4switch.go
@@ -1,6 +1,5 @@
-/*Go's switch is more general than C's. The expressions need not be constants or even integers, 
- the cases are evaluated top to bottom until a match is found, and if the switch has no expression it switches on true.
- It's therefore possible—and idiomatic—to write an if-else-if-else chain as a switch. */
+/* Go's switch is more general than C's. The expressions need not be constants or even integers, the cases are evaluated top to bottom until a match is found, 
+  and if the switch has no expression it switches on true. It's therefore possible—and idiomatic—to write an if-else-if-else chain as a switch. */
 
 package main
 
@@ -13,22 +12,21 @@ func main(){
 	//the break statement is provided automatically in Go after every case.
 	
 	fmt.Println("When's Saturday?")
-	
 	today := time.Now().Weekday()
-	
 	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today.")
-	case today + 1:
-		fmt.Println("Tomorrow.")
-	case today + 2:
-		fmt.Println("In two days.")
-	default:
-		fmt.Println("Too far away.")
-
-		//cases can be presented in comma-separated lists.
-		switch c {
+		case today + 0:
+			fmt.Println("Today.")
+		case today + 1:
+			fmt.Println("Tomorrow.")
+		case today + 2:
+			fmt.Println("In two days.")
+		default:
+			fmt.Println("Too far away.")
+	}
+	//cases can be presented in comma-separated lists.
+	switch c {
 		case ' ', '?', '&', '=', '#', '+', '%':
 			return true
 		}
 	}
+}
