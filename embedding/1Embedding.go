cf601f21bbcf3b732a9fb950dd7aeba493a4c423
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-31 10:40:53 +0530

    indent code

--- embedding/1Embedding.go
+++ embedding/1Embedding.go
@@ -1,25 +1,11 @@
 /* * Go does not provide the typical, type-driven notion of subclassing, but it does have the ability to “borrow” pieces 
      of an implementation by embedding types within a struct or interface.
-   * The embedded elements are pointers to structs and of course must be initialized to point to valid structs before they can be used 
+   * The embedded elements are pointers to structs and of course must be initialized to point to valid structs before they can be used   */
 
+package main
 
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-
-
-package main()
 import "fmt"
+
 type person struct{
 	name,add string;
 	phone int
@@ -33,15 +19,15 @@ func (E *employe) setInfo(){
 	fmt.Scan(&E.name,&E.add,&E.phone,&E.salary,&E.dept,&E.id);
 }
 func (E employe) getInfo(){
-	 fmt.Println("Name   	: ",E.name)
-	 fmt.Println("Id     	: ",E.id)
-	 fmt.Println("Dept   	: ",E.dept)
-	 fmt.Println("Salary 	: ",E.salary)
-	 fmt.Println("Phone No. : ",E.phone)
-	 fmt.Println("Adress    : ",E.add)
+	fmt.Println("Name   	: ",E.name)
+	fmt.Println("Id     	: ",E.id)
+	fmt.Println("Dept   	: ",E.dept)
+	fmt.Println("Salary 	: ",E.salary)
+	fmt.Println("Phone No. : ",E.phone)
+	fmt.Println("Adress    : ",E.add)
 }
 func main(){
-      emp:=employee
-	  emp.setInfo()
-	  emp.getInfo()
+      	emp:=employee
+	emp.setInfo()
+	emp.getInfo()
 }
