9667f6713196505c36b0d9e2cff5376692d8cb66
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-31 09:43:05 +0530

    code

--- blank identifier/9.1multipleAssignment.go
+++ blank identifier/9.1multipleAssignment.go
@@ -1,7 +1,7 @@
 /* * We've mentioned the blank identifier a couple of times now, in the context of for range loops and maps. The blank identifier can be
      assigned or declared with any value of any type, with the value discarded harmlessl.
 
-	The blank identifier in multiple assignment :
+    * The blank identifier in multiple assignment :
 		* The use of a blank identifier in a for range loop is a special case of a general situation: multiple assignment.
 		* If an assignment requires multiple values on the left side, but one of the values will not be used by the program, 
 		  a blank identifier on the left-hand-side of the assignment avoids the need to create a dummy variable and makes it 
@@ -13,10 +13,10 @@ func main(){
 	// a simplew use of blank identifier
 	//here if we use any general variable then it will cause error
 	//because its declared but not used
-	_,x:=isPrime(16)
+	_ , x:=isPrime(16)
 	switch x {
-	case true:fmt.Println("its a Prime Number")
-	case false:fmt.Println("its not a Prime Number")
+		case true:fmt.Println("its a Prime Number")
+		case false:fmt.Println("its not a Prime Number")
 	}
 }
 
