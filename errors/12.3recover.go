72e131bb7daee43b2e665410e83677c96dffb745
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-31 10:53:45 +0530

    indent code

--- errors/12.3recover.go
+++ errors/12.3recover.go
@@ -1,25 +1,21 @@
-/* * When panic is called, including implicitly for run-time errors such as indexing a slice out of bounds or failing a type assertion, 
-     it immediately stops execution of the current function and begins unwinding the stack of the goroutine, running any deferred functions
-	 along the way.If that unwinding reaches the top of the goroutine's stack, the program dies.
+/* * When panic is called, including implicitly for run-time errors such as indexing a slice out of bounds or failing a type assertion, it immediately stops execution 
+     of the current function and begins unwinding the stack of the goroutine, running any deferred functions along the way.If that unwinding reaches the top of the 
+     goroutine's stack, the program dies.
    * It is possible to use the built-in function recover to regain control of the goroutine and resume normal execution.
    * A call to recover stops the unwinding and returns the argument passed to panic. Because the only code that runs while unwinding
      is inside deferred functions.
-NOTE : recover is only useful inside deferred functions.If the recover function is not within the defer function then it will not stop panic
- */
-
-
-
+NOTE : recover is only useful inside deferred functions.If the recover function is not within the defer function then it will not stop panic */
 
 package main
+
 import "fmt"
+
 func main(){
 	a:=[]string{"one","two"}
 	checkAndPrint(a,2)
 }
-
 //checkAndPrint which checks and prints slice element at an index passed in the argument
-func checkAndPrint(a []string,index int){
-	
+func checkAndPrint(a []string,index int){	
 	//defer function named handleOutIfBounds as well at the start of the function checkAndPrint.
 	//This function  contains  the  call to recover function
 	defer handleOutOfBound();
@@ -30,7 +26,6 @@ func checkAndPrint(a []string,index int){
 	}
 	fmt.Println(a[index]);
 }
-
 func handleOutOfBound(){
 	if r:=recover();r!=nil{
 		//The recover function will catch the panic and we can also print the message from the panic. 
