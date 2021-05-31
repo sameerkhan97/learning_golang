b4a019ec190d086ee4f4c4b3f980d040db069ec3
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-31 11:00:05 +0530

    indent code

--- initialization/6.1constant.go
+++ initialization/6.1constant.go
@@ -1,18 +1,15 @@
-/*	* Constants in Go are created at compile time, even when defined as locals in functions.
-	
-	* Constants can only be numbers, characters (runes), strings or booleans.
-	
-	* Because of the compile-time restriction, the expressions that define them must be constant expressions, evaluatable by the compiler
-	  for example : 1<<3 is a constant expression, while math.Sin(math.Pi/4) is not because the function call to math.Sin needs to happen
-	  at run time.
-	
-    * In Go, enumerated constants are created using the iota enumerator. Since iota can be part of an expression and expressions can 
-	  be implicitly repeated, it is easy to build intricate sets of values.
-	
-	* use 'const' keyword to declare constant
-*/
+/* * Constants in Go are created at compile time, even when defined as locals in functions.
+   * Constants can only be numbers, characters (runes), strings or booleans.	
+   * Because of the compile-time restriction, the expressions that define them must be constant expressions, evaluatable by the compiler for example : 1<<3 is a constant
+     expression, while math.Sin(math.Pi/4) is not because the function call to math.Sin needs to happen at run time.
+   * In Go, enumerated constants are created using the iota enumerator. Since iota can be part of an expression and expressions can be implicitly repeated, it is easy to
+     build intricate sets of values.
+   * use 'const' keyword to declare constant   */
+
 package main
+
 import "fmt"
+
 func main(){
 	const a int64 = 42				//TYPED CONSTANT
 	fmt.Printf("%v,%T\n",a,a)
