50181c50002024527c16159ec833b8dd46255327
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-31 10:49:04 +0530

    indent code

--- errors/12.1errors.go
+++ errors/12.1errors.go
@@ -2,18 +2,19 @@
    * Go's multivalue return makes it easy to return a detailed error description alongside the normal return value
    * For example, as we'll see, os.Open doesn't just return a nil pointer on failure, 
      it also returns an error value that describes what went wrong.
-   * By convention, errors have type error, a simple built-in interface.
-		
+   * By convention, errors have type error, a simple built-in interface.	
   		type error interface {
     		Error() string
 		}
 */
 
 package main
+
 import (
 	"fmt"
 	"errors"
 )
+
 func main(){
 	//sumOf() calculate sum of elements between given range
 	//it will give error when start of range is greater than end of range  
