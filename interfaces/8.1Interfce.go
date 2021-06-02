3803400c5928910f06ce81b6eb1a86f05bea7a33
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-06-02 20:45:16 +0530

    format code

--- interfaces/8.1Interfce.go
+++ interfaces/8.1Interfce.go
@@ -1,34 +1,35 @@
 package main
+
 import "fmt"
 
-type shape interface{
+type shape interface {
 	Area() float64
 }
-type square struct{
+type square struct {
 	side float64
 }
+
 //here square implementing shape interface by defining its function area in it
-func (s square) Area() float64{
-	return s.side*s.side
+func (s square) Area() float64 {
+	return s.side * s.side
 }
 
-type circle struct{
+type circle struct {
 	radius float64
 }
 
 //here circle implementing shape interface by defining its function area in it
-func (c circle) Area() float64{
-	return 3.14*c.radius*c.radius
+func (c circle) Area() float64 {
+	return 3.14 * c.radius * c.radius
 }
-func main(){
-	circle:=circle{10.0}
-	square:=square{2.0}
+func main() {
+	circle := circle{10.0}
+	square := square{2.0}
 	getAreaOf(circle)
 	getAreaOf(square)
 }
 
 //the shape can call function using any object ht implement its Area function
 func getAreaOf(sh shape) {
-	fmt.Println("Area ",sh.Area())
+	fmt.Println("Area ", sh.Area())
 }
-
