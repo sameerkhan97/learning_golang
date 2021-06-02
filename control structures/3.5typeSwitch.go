f045aeae218dabe3145798702d61f2e434384100
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-06-02 20:15:20 +0530

    format code

--- control structures/3.5typeSwitch.go
+++ control structures/3.5typeSwitch.go
@@ -5,20 +5,20 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
 	var t interface{}
 	t = functionOfSomeType()
-	switch t := t.(type) {	// reuse the name in such cases, in effect declaring a new variable with the same name but a different type in each case.
-		default:
-			fmt.Printf("unexpected type %T\n", t)     // %T prints whatever type t has
-		case bool:
-			fmt.Printf("boolean %t\n", t)             // t has type bool
-		case int:
-			fmt.Printf("integer %d\n", t)             // t has type int
-		case *bool:
-		    	fmt.Printf("pointer to boolean %t\n", *t) // t has type *bool
-		case *int:
-		    	fmt.Printf("pointer to integer %d\n", *t) // t has type *int
+	switch t := t.(type) { // reuse the name in such cases, in effect declaring a new variable with the same name but a different type in each case.
+	default:
+		fmt.Printf("unexpected type %T\n", t) // %T prints whatever type t has
+	case bool:
+		fmt.Printf("boolean %t\n", t) // t has type bool
+	case int:
+		fmt.Printf("integer %d\n", t) // t has type int
+	case *bool:
+		fmt.Printf("pointer to boolean %t\n", *t) // t has type *bool
+	case *int:
+		fmt.Printf("pointer to integer %d\n", *t) // t has type *int
 	}
 }
