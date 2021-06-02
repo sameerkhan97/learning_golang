35a420159bf08a9850c0324585a65eaf6e8104b2
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-06-02 19:44:34 +0530

    format code

--- concurrency/11.4blockOnSendandRecieveOnChannel.go
+++ concurrency/11.4blockOnSendandRecieveOnChannel.go
@@ -2,25 +2,28 @@
 
    * The receive on a channel is blocked unless there is another goroutine to send to that channel.
 */
+
 package main
+
 import (
 	"fmt"
 	"time"
 )
-func main(){
-	a:=make(chan int)	//defining channel using function make.
+
+func main() {
+	a := make(chan int) //defining channel using function make.
 	fmt.Println("Sending values to channels")
 	go send(a)
 	fmt.Println("Recieving values to channels")
 	go recieve(a)
-	time.Sleep(time.Second*2);
+	time.Sleep(time.Second * 2)
 }
-func send(ch chan int){
-	ch<-1
+func send(ch chan int) {
+	ch <- 1
 	fmt.Println("Sending value to channel complete")
 }
-func recieve(ch chan int){
-	time.Sleep(time.Second * 1)	//here recieve function is stop for some seconds hence send() is blocked from executing and after seconds recieve start
-	val:=<-ch
-	fmt.Println("Value recieved = ",val," in recieve function")
+func recieve(ch chan int) {
+	time.Sleep(time.Second * 1) //here recieve function is stop for some seconds hence send() is blocked from executing and after seconds recieve start
+	val := <-ch
+	fmt.Println("Value recieved = ", val, " in recieve function")
 }
