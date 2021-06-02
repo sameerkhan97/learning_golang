c4cba944da0e3761fd46e1218b9fcaffdb479829
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-06-02 20:44:41 +0530

    format code

--- initialization/6.3init.go
+++ initialization/6.3init.go
@@ -2,22 +2,24 @@
    * Each file can have multiple init functions
    * Multiple init() functio are be called one after another
    * init() is called after all the variable declarations in the package have evaluated their initializers,
-     and those are evaluated only after all the imported packages have been initialized. 
+     and those are evaluated only after all the imported packages have been initialized.
    * init() is used to do task thgat we want to do before everythinng else.
-	*/
+*/
 
 package main
+
 import "fmt"
-func init(){
+
+func init() {
 	fmt.Println("In init 1 ")
 }
-func init(){
+func init() {
 	fmt.Println("In init 2 ")
 }
-func main(){
-	a:=10;
-	fmt.Println("In Main ",a)
-} 
-func init(){
+func main() {
+	a := 10
+	fmt.Println("In Main ", a)
+}
+func init() {
 	fmt.Println("In init 3 ")
 }
