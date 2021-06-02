34839cca85cae95f27fefb17cd0522dfc8b6c17e
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-06-02 20:43:26 +0530

    format code

--- initialization/6.1constant.go
+++ initialization/6.1constant.go
@@ -1,5 +1,5 @@
 /* * Constants in Go are created at compile time, even when defined as locals in functions.
-   * Constants can only be numbers, characters (runes), strings or booleans.	
+   * Constants can only be numbers, characters (runes), strings or booleans.
    * Because of the compile-time restriction, the expressions that define them must be constant expressions, evaluatable by the compiler for example : 1<<3 is a constant
      expression, while math.Sin(math.Pi/4) is not because the function call to math.Sin needs to happen at run time.
    * In Go, enumerated constants are created using the iota enumerator. Since iota can be part of an expression and expressions can be implicitly repeated, it is easy to
@@ -10,14 +10,14 @@ package main
 
 import "fmt"
 
-func main(){
-	const a int64 = 42				//TYPED CONSTANT
-	fmt.Printf("%v,%T\n",a,a)
+func main() {
+	const a int64 = 42 //TYPED CONSTANT
+	fmt.Printf("%v,%T\n", a, a)
 
-	const b=4.12;					//UNTYPED CONSTANT
-	fmt.Printf("%v,%T\n",b,b)
-	//statement a=a+1/b=b+1 cause error bcoz we cant modify constants 
+	const b = 4.12 //UNTYPED CONSTANT
+	fmt.Printf("%v,%T\n", b, b)
+	//statement a=a+1/b=b+1 cause error bcoz we cant modify constants
 
-	const c=false;					//UNTYPED CONSTANT
-	fmt.Printf("%v,%T\n",c,c)
+	const c = false //UNTYPED CONSTANT
+	fmt.Printf("%v,%T\n", c, c)
 }
