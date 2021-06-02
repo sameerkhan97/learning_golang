bbc4985bfa5d53d785c7b7d33d9d16c49bca3848
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-06-02 20:17:34 +0530

    format code

--- data/5.3Array.go
+++ data/5.3Array.go
@@ -6,27 +6,29 @@
 			~The size of an array is part of its type. The types [10]int and [20]int are distinct						*/ 
 
 package main
+
 import "fmt"
-func main(){
+
+func main() {
 	//simple declaration of an array in GO
-	v:= [10]int{1,2,3,4,5,6,7,8,9,10}	
-	fmt.Println("v :",v);
-	
-	y:=sum(&v)			//passing array to a function by reference
-	fmt.Println("Sum : ",y)
-	fmt.Println("v :",v)
+	v := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("v :", v)
+
+	y := sum(&v) //passing array to a function by reference
+	fmt.Println("Sum : ", y)
+	fmt.Println("v :", v)
 
 	//copying an array
-	x:=v
-	fmt.Println("x :",x)
-	x[2]=99				//changing value in copy array , will it effect on v?
-	fmt.Println("v :",v)
-	fmt.Println("x :",x)
+	x := v
+	fmt.Println("x :", x)
+	x[2] = 99 //changing value in copy array , will it effect on v?
+	fmt.Println("v :", v)
+	fmt.Println("x :", x)
 }
-func sum(a *[10]int) (sum int){
-	for i:=0;i<len(a);i++{
-		sum=sum+a[i];
+func sum(a *[10]int) (sum int) {
+	for i := 0; i < len(a); i++ {
+		sum = sum + a[i]
 	}
-	a[0]=100;			//changiing values of array in function to check if it effect v
-	return 
+	a[0] = 100 //changiing values of array in function to check if it effect v
+	return
 }
