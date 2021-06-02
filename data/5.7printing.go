a1679617b1bb4da4da58d7bb6e17b7ded24f2035
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-06-02 20:36:47 +0530

    format code

--- data/5.7printing.go
+++ data/5.7printing.go
@@ -16,15 +16,15 @@ type T struct {
 	c string
 }
 
-func main(){
-	fmt.Printf("Hello %d\n", 23)			//Print versions add blanks only if the operand on neither side is a string.
-	fmt.Println("Hello", 23)			//Println insert a blank between arguments and append a newline to the output
-	fmt.Println(fmt.Sprint("Hello ", 23))		//Sprintf return a string rather than filling in a provided buffer
-	fmt.Printf("%T",12.46)				//prints the type of variable
+func main() {
+	fmt.Printf("Hello %d\n", 23)          //Print versions add blanks only if the operand on neither side is a string.
+	fmt.Println("Hello", 23)              //Println insert a blank between arguments and append a newline to the output
+	fmt.Println(fmt.Sprint("Hello ", 23)) //Sprintf return a string rather than filling in a provided buffer
+	fmt.Printf("%T", 12.46)               //prints the type of variable
 
 	//printing structure
-	t := T{ 7, -2.35, "abc\tdef" }
-	fmt.Printf("%v\n", t) 		
-	fmt.Printf("%+v\n", t)				//%+v annotates the fields of the structure with their names
-	fmt.Printf("%#v\n", t)				// for any value the alternate format %#v prints the value in full Go syntax.
-}	 
+	t := T{7, -2.35, "abc\tdef"}
+	fmt.Printf("%v\n", t)
+	fmt.Printf("%+v\n", t) //%+v annotates the fields of the structure with their names
+	fmt.Printf("%#v\n", t) // for any value the alternate format %#v prints the value in full Go syntax.
+}
