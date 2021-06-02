9f81331130d4f02a8394b879e5b3e49ca9c5df48
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-06-02 20:42:26 +0530

    format code

--- functions/4.2namedReturnParameters.go
+++ functions/4.2namedReturnParameters.go
@@ -1,4 +1,4 @@
-/* The return or result "parameters" of a Go function can be given names and used as regular variables, just like the incoming parameters. When named, they are initialized 
+/* The return or result "parameters" of a Go function can be given names and used as regular variables, just like the incoming parameters. When named, they are initialized
    to the zero values for their types when the function begins; if the function executes a return statement with no arguments, the current values of the result parameters
    are used as the returned values.*/
 
@@ -6,12 +6,12 @@ package main
 
 import "fmt"
 
-func main(){
-	x,y:=92,302;
-	sum:=add(x,y);
-	fmt.Println(x," + ",y," = " ,sum);
+func main() {
+	x, y := 92, 302
+	sum := add(x, y)
+	fmt.Println(x, " + ", y, " = ", sum)
 }
-func add(a,b int) (sum int){
-	sum=a+b;
-	return 
+func add(a, b int) (sum int) {
+	sum = a + b
+	return
 }
