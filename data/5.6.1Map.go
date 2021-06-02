e90cd9c8f1924ab1d8e09714a2267417f7319553
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-06-02 20:35:27 +0530

    format code

--- data/5.6.1Map.go
+++ data/5.6.1Map.go
@@ -15,54 +15,56 @@
 */
 
 package main
+
 import "fmt"
-func main(){
-	statePop:=map[string]int{	//key of type string and value of type int
-		"California"   : 31654 ,
-		"Texas"       : 23145 ,
-		"Florida"     : 54987 ,
-		"New York"    : 71475 ,
-		"Pennsylvania" : 36841 ,
-		"Illinois"    : 16414 ,
-		"Ohio"        : 46345 ,
+
+func main() {
+	statePop := map[string]int{ //key of type string and value of type int
+		"California":   31654,
+		"Texas":        23145,
+		"Florida":      54987,
+		"New York":     71475,
+		"Pennsylvania": 36841,
+		"Illinois":     16414,
+		"Ohio":         46345,
 	}
 	fmt.Println(statePop)
 	//another declaration using make method
-	statePop1:=make(map[string]int,10)
-	statePop1=map[string]int{
-		"California"   : 31654 ,
-		"Texas "       : 23145 ,
-		"Florida "     : 54987 ,
-		"New York "    : 71475 ,
-		"Pennsylvania" : 36841 ,
-		"Illinois "    : 16414 ,
-		"Ohio "        : 46345 ,
+	statePop1 := make(map[string]int, 10)
+	statePop1 = map[string]int{
+		"California":   31654,
+		"Texas ":       23145,
+		"Florida ":     54987,
+		"New York ":    71475,
+		"Pennsylvania": 36841,
+		"Illinois ":    16414,
+		"Ohio ":        46345,
 	}
 	fmt.Println(statePop1)
-	//Note : arrays can be used as a key-type but not map 
-	m:=map[[3]int]string{}
-	fmt.Println(statePop,m)	
+	//Note : arrays can be used as a key-type but not map
+	m := map[[3]int]string{}
+	fmt.Println(statePop, m)
 
-	//manipulation : 
-	fmt.Println("Populataion of Ohio " ,statePop["Ohio"])
-	statePop["Ohio"]=78964
-	fmt.Println("New Populataion of Ohio " ,statePop["Ohio"])
-	
+	//manipulation :
+	fmt.Println("Populataion of Ohio ", statePop["Ohio"])
+	statePop["Ohio"] = 78964
+	fmt.Println("New Populataion of Ohio ", statePop["Ohio"])
+
 	//checking if key is present or not
 	fmt.Println("Searching in Map")
-	pop,ok:=statePop["Delhi"];
-	fmt.Println(pop,ok)
-	pop,ok=statePop["New York"];
-	fmt.Println(pop,ok)
+	pop, ok := statePop["Delhi"]
+	fmt.Println(pop, ok)
+	pop, ok = statePop["New York"]
+	fmt.Println(pop, ok)
 
 	//size of map
-	fmt.Println("No of Cities : ",len(statePop))
+	fmt.Println("No of Cities : ", len(statePop))
 
 	//deleting from map
 	fmt.Println("deleting from map")
-	sp:=statePop;
+	sp := statePop
 	fmt.Println(sp)
-	delete(sp,"Ohio")
+	delete(sp, "Ohio")
 	fmt.Println(sp)
 	fmt.Println(statePop)
 
